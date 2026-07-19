cc713f858b13fac382292872523daa8095b46c3d
Pat <whyyesmommy@gmail.com>
2026-07-19 23:24:43 +0700

    Update LAB01.py
    Added dataset exploration and cleaning steps.

--- LAB01.py
+++ LAB01.py
@@ -1,6 +1,16 @@
 import pandas as pd
+
+#Dataset Exploration
 df = pd.read_csv(r'c:\Users\Ansel\Documents\Uni\Class work\cow_milk_mastitis_dataset.csv')
+print(df.shape)
+print(df.dtypes)
+print(df.describe())
+print(df.isnull().sum())
+print("Duplicated columns", df.columns.duplicated())
+
+#Dataset Cleaning
 df = df.drop_duplicates()
 df = df.drop(columns=['class1'])
 df["Clotting"] = df["Clotting"].astype(bool)
-print(df.to_string())
+print(df.Milk_Conductivity.describe())
+#print(df.to_string())
