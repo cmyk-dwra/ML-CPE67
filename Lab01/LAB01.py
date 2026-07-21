c2e610f7a768e17f6b56de21ecd3a5ff4bfbe886
cmyk-dwra <whyyesmommy@gmail.com>
2026-07-21 12:29:43 +0700

    Imported seaborn, moved cow_milk_mastitis_dataset.csv to appropriate folder

--- Lab01/LAB01.py
+++ Lab01/LAB01.py
@@ -1,16 +1,17 @@
 import pandas as pd
+import seaborn as sns
 
 #Dataset Exploration
-df = pd.read_csv(r'c:\Users\Ansel\Documents\Uni\Class work\cow_milk_mastitis_dataset.csv')
-print(df.shape)
-print(df.dtypes)
-print(df.describe())
-print(df.isnull().sum())
-print("Duplicated columns", df.columns.duplicated())
+df = pd.read_csv("cow_milk_mastitis_dataset.csv")
+#sns.jointplot(data=df, x = "Milk_Conductivity", y = "Clotting", hue = "class1")
+#print(df)
+print("Data types\n", df.dtypes, "\n")
+print("Summary \n", df.describe(), "\n")
+print("Is null\n", df.isnull().sum(), "\n")
+print("Duplicated columns\n", df.columns.duplicated(), "\n")
 
 #Dataset Cleaning
 df = df.drop_duplicates()
 df = df.drop(columns=['class1'])
 df["Clotting"] = df["Clotting"].astype(bool)
-print(df.Milk_Conductivity.describe())
-#print(df.to_string())
+print(df.to_string())
