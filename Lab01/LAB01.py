676fc16dd5128e970443c37611c1c5ecfbe1b21f
cmyk-dwra <whyyesmommy@gmail.com>
2026-07-21 18:31:23 +0700

    Formatted display.

--- Lab01/LAB01.py
+++ Lab01/LAB01.py
@@ -2,21 +2,21 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-Path = "C:/Users/Ansel/Desktop/Ansel/Python/ML-CPE67/"
+Path = "C:/Users/Ansel/Desktop/Ansel/Python/ML-CPE67/Lab01/"
 #Dataset Exploration
-df = pd.read_csv(f"{Path}/Lab01/CowMilkMastitisDataset.csv")
+df = pd.read_csv(f"{Path}CowMilkMastitisDataset.csv")
 #sns.jointplot(data=df, x = "Milk_Conductivity", y = "Clotting", hue = "class1")
 #print(df)
-print("Data types\n", df.dtypes, "\n")
-print("Summary \n", df.describe(), "\n")
-print("Is null\n", df.isnull().sum(), "\n")
-print("Duplicated columns\n", df.columns.duplicated(), "\n")
+print("Data Types vvvvvvvvvvvvvvvvvvv \n", df.dtypes, "\n")
+print("Data Summary vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv \n", df.describe(), "\n")
+print("Rows with null values: ", df.isnull().all(axis=1).sum(), "\n")
+print("Duplicated data: ", df.duplicated().sum(),"\n")
 
 #Dataset Cleaning
 df = df.drop_duplicates()
 df = df.drop(columns=['class1'])
 df["Clotting"] = df["Clotting"].astype(bool)
-print(df.to_string())
+print("Data vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv \n", df.to_string())
 
 #Data Visualization
 sns.histplot(data=df, x = "Milk_Yield", multiple = "stack", color = 'm')
