1148da4e7780914a976500f25fe92e2d4a37286f
cmyk-dwra <whyyesmommy@gmail.com>
2026-07-21 19:50:21 +0700

    Do Data Cleaning first then Data Exploration. Improved format.

--- Lab01/LAB01.py
+++ Lab01/LAB01.py
@@ -3,20 +3,24 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 Path = "C:/Users/Ansel/Desktop/Ansel/Python/ML-CPE67/Lab01/"
-#Dataset Exploration
 df = pd.read_csv(f"{Path}CowMilkMastitisDataset.csv")
-#sns.jointplot(data=df, x = "Milk_Conductivity", y = "Clotting", hue = "class1")
-#print(df)
-print("Data Types vvvvvvvvvvvvvvvvvvv \n", df.dtypes, "\n")
-print("Data Summary vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv \n", df.describe(), "\n")
-print("Rows with null values: ", df.isnull().all(axis=1).sum(), "\n")
-print("Duplicated data: ", df.duplicated().sum(),"\n")
 
 #Dataset Cleaning
 df = df.drop_duplicates()
 df = df.drop(columns=['class1'])
 df["Clotting"] = df["Clotting"].astype(bool)
-print("Data vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv \n", df.to_string())
+
+#Dataset Exploration
+Summary = df.describe()
+#sns.jointplot(data=df, x = "Milk_Conductivity", y = "Clotting", hue = "class1")
+#print(df)
+print("Data Types", "-"*100, "\n", df.dtypes, "\n")
+print("Data Summary", "-"*100, "\n", Summary, "\n")
+print("Mean:Median ratio", "-"*100, "\n",Summary.loc['mean']/df.select_dtypes(include=['int64', 'float64']).median(), "\n", "-"*120, "\n")
+print("Rows with null values: ", df.isnull().all(axis=1).sum(), "\n")
+print("Duplicated data: ", df.duplicated().sum(),"\n")
+
+print("Data", "-"*100, "\n", df.to_string(), "\n", "-"*120)
 
 #Data Visualization
 sns.histplot(data=df, x = "Milk_Yield", multiple = "stack", color = 'm')
