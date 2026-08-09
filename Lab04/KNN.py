c8096413226381d30d9429dd1957602bf97cea62
cmyk-dwra <whyyesmommy@gmail.com>
2026-08-09 18:22:38 +0700

    KNN(regressor) model for predicting milk yield

--- Lab04/KNN.py
+++ Lab04/KNN.py
@@ -1,24 +1,33 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.metrics import accuracy_score
 
 Path = "C:/Users/Ansel/Desktop/Ansel/Python/ML-CPE67"
-df = pd.read_csv(f"{Path}/Lab02/CowMilkMastitisDataset.csv")
+df = pd.read_csv(f"{Path}/Lab04/CowMilkMastitisDataset.csv")
 
-#Dataset Exploration
-Summary = df.describe()
-#sns.jointplot(data=df, x = "Milk_Conductivity", y = "Clotting", hue = "class1")
-#print(df)
-print("Data Types", "-"*100, "\n", df.dtypes, "\n")
-print("Data Summary", "-"*100, "\n", Summary, "\n")
-print("Mean:Median ratio", "-"*100, "\n",Summary.loc['mean']/df.select_dtypes(include=['int64', 'float64']).median(), "\n", "-"*120, "\n")
-print("Rows with null values: ", df.isnull().all(axis=1).sum(), "\n")
-print("Duplicated data: ", df.duplicated().sum(),"\n")
-
-print("Data", "-"*100, "\n", df.to_string(), "\n", "-"*120)
-
-#Classification
-X = df.drop(columns=["class1"])
-Y = df["class1"]
+X = df.drop(columns=["class1", "Cow_ID", "Day", "Milk_Yield"]) #Prediction features
+Y = df["Milk_Yield"] #Prediction target
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test) #Scaling and unifying the feature data
+
+knn = KNeighborsRegressor(n_neighbors=5)
+knn.fit(X_train, Y_train) #Training the KNN model
+
+Y_pred = knn.predict(X_test) #Predicting the target values using the KNN model
+result = pd.DataFrame({"Actual": Y_test, "Predicted": Y_pred})
+print("Training:", len(X_train))
+print("Testing:", len(X_test))
+accuracy = 1 - np.abs(Y_pred - Y_test) / np.abs(Y_test)
+
+for actual, predicted in zip(Y_test, Y_pred):
+    accuracy = 1 - abs(predicted - actual) / abs(actual)
+    print(f"Actual: {actual}, Predicted: {predicted:.2f}, Accuracy: {accuracy * 100:.2f}%")
+print("Mean Accuracy:", accuracy.mean() * 100, "%")
